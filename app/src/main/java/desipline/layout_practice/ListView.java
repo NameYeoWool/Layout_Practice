90f8c25acd62a3b619691d09bbe6797fc5579f71
YeoWool <gkaudrl542@daum.net>
2018-08-08 14:13:14 +0900

    another example

--- app/src/main/java/desipline/layout_practice/ListView.java
+++ app/src/main/java/desipline/layout_practice/ListView.java
@@ -2,31 +2,37 @@ package desipline.layout_practice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ListView extends AppCompatActivity {
 
-    android.widget.ListView listView;
-    ArrayAdapter<String> mAdapter;
+    private ArrayList<HashMap<String,String>> Data = new ArrayList<HashMap<String, String>>();
+    private HashMap<String,String> InputData1 = new HashMap<>();
+    private HashMap<String,String> InputData2 = new HashMap<>();
+    private android.widget.ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        listView = (android.widget.ListView)findViewById(R.id.listView1);
 
-        listView = (android.widget.ListView) findViewById(R.id.listView1);
-        mAdapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item, new ArrayList<String>());
-        listView.setAdapter(mAdapter);
-        initData();
+        InputData1.put("school","서울대");
+        InputData1.put("name", "우혁");
+        Data.add(InputData1);
+
+        InputData2.put("school", "연세대");
+        InputData2.put("name", "우재석");
+        Data.add(InputData2);
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,Data,android.R.layout.simple_list_item_2,
+                new String[]{"school","name"},new int[]{android.R.id.text1,android.R.id.text2});
+        listView.setAdapter(simpleAdapter);
+
     }
 
-    private void initData(){
-        String[] items = getResources().getStringArray(R.array.list_item);
-        for(String s:items){
-            mAdapter.add(s);
-        }
-    }
 }
